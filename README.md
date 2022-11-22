a2c6366253a5398e5046a5529f43a43ff03edbaa
Kalin Staykov <k.t.staykov@gmail.com>
2022-11-22 22:11:37 +0200

    implement initial gRPC server

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ A basic tripwire system that will hash all files in given path and send the inte
 
 In lockdown mode it will alert the server if changes to the filesystem are observed.
 
+# Send data to the gRPC server via Connect using curl
+
+```
+curl \
+    --header "Content-Type: application/json" \
+    --data '{"file_path": "myfile", "file_hash": "myhash"}' \
+    http://localhost:8080/tripserv.v1.TripnetService/FileHash
+```
+
 # State of the project
 
 It's still WIP and most functionality is not developed yet.
---
+++ cmd/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	tripservv1 "tripnet/gen/pkg/tripserv/v1"
+	"tripnet/gen/pkg/tripserv/v1/tripservv1connect"
+
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type TripServer struct{}
+
+func (s *TripServer) FileHash(
+	ctx context.Context,
+	req *connect.Request[tripservv1.FileHashRequest],
+) (*connect.Response[tripservv1.FileHashResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&tripservv1.FileHashResponse{
+		Status: "received hash " + req.Msg.FileHash,
+	})
+	res.Header().Set("Tripserv-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	tripserv := &TripServer{}
+	mux := http.NewServeMux()
+	path, handler := tripservv1connect.NewTripnetServiceHandler(tripserv)
+	mux.Handle(path, handler)
+	fmt.Println("starting server..")
+	http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
